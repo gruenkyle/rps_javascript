f22a2f300a3f57be92271ca961a183f1e3fa107f
gruenkyle <gruenkyle@vt.edu>
2022-10-25 16:28:56 -0400

    Game Logic Function Created

--- external.js
+++ external.js
@@ -1,1 +1,49 @@
 alert("I am Functioning!"); 
+
+//We Must Create our array variable first of course//
+const arr = ["rock", "paper", "scissors"];
+
+//Next we create a function that will get computer choice//
+
+function getComputerChoice(){
+    let random = arr[Math.floor(Math.random() * arr.length)]; 
+    return (random);
+}
+
+//After we make sure the random choice works we can now get results//
+function roundPlay(selection, computerSelection){
+    let playerSelection = selection.toLowerCase(); 
+
+    if (playerSelection == computerSelection){
+        return "tie";
+    }
+    else if (playerSelection == 'rock'){
+        if (computerSelection == 'paper'){
+            return "lose";
+        }
+        else{
+            return "win";
+        }
+    }
+    else if (playerSelection == 'paper'){
+        if (computerSelection == 'scissors'){
+            return "lose";
+        }
+        else {
+            return "win";
+        }
+    }
+    else if (playerSelection == 'scissors'){
+        if (computerSelection == 'rock'){
+            return "lose";
+        }
+        else {
+            return 'win'; 
+        }
+    }
+    else {
+        return "invalid"
+    }
+
+
+}
